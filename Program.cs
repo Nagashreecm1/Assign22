cabdafaf757af06cfbf3777a5fe61d4c0b7d1247
nagashreecm <nagashreemanvitha502@gmail.com>
2023-08-03 22:28:51 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Source source = new Source
+        {
+            CommonProperty1 = 10,
+            CommonProperty2 = "Hello",
+            CommonProperty3 = 3.14
+        };
+
+        Destination destination = new Destination();
+
+        PropertyMapper.MapProperties(source, destination);
+
+        Console.WriteLine($"CommonProperty1 in Destination: {destination.CommonProperty1}");
+        Console.WriteLine($"CommonProperty2 in Destination: {destination.CommonProperty2}");
+        Console.WriteLine($"DestinationProperty1 in Destination: {destination.DestinationProperty1}");
+    }
+}
